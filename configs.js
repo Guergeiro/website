0a761faa3a5c9dc117770d369852c7d3131b535e
crookse <crookse@users.noreply.github.com>
2021-03-28 00:52:30 +0000

    Update dependencies and bump version numbers across repo

--- configs.js
+++ configs.js
@@ -2,10 +2,10 @@ export const configs = {
   "copyright_year": "2021",
   "root_directory": ".",
   "deno": {
-    "latest_version": "v1.8.1"
+    "latest_version": "v1.8.2"
   },
   "deno_std": {
-    "latest_version": "0.90.0"
+    "latest_version": "0.91.0"
   },
   "dmm": {
     "base_url": "/dmm/v1.x",
